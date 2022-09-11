d9abf025c5fb2d5571f9f6d347c5448e74f8bb5d
nishchay <rathi17nishchay@gmail.com>
2022-09-11 13:54:09 +0530

    css is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Note from './components/Note';
 
 
 function App(){
@@ -8,6 +9,7 @@ function App(){
 	return (
 		<div className="app">
 			<Header />
+			<Note />
 			<Footer />
 		</div> 	
 	);
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,12 @@ console.log(date);
 
 function Footer(){
 	
+	var date = new Date();
+	var year = date.getFullYear();
+	
 	return (
 		<footer className='footer'>
-			<p>Copyright &copy; </p>
+			<p>Copyright &copy; {year}</p>
 		</footer>
 	);
 	
